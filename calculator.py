eb3c0fc594e46a4d08be33397ef8a02b1623ae9b
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-05-14 20:39:39 +0200

    Identifying Whether the User Has Finished

--- calculator.py
+++ calculator.py
@@ -23,6 +23,24 @@ def modulus(x, y):
     return x % y
 
 
+def check_if_user_has_finished():
+    """
+    Checks that the user wants to finish or not.
+    Performs some verification of the input."""
+    ok_to_finish = True
+    user_input_accepted = False
+    while not user_input_accepted:
+        user_input = input('Do you want to finish (y/n): ').lower()
+        if user_input == 'y':
+            user_input_accepted = True
+        elif user_input == 'n':
+            ok_to_finish = False
+            user_input_accepted = True
+        else:
+            print('Response must be (y/n), please try again')
+    return ok_to_finish
+
+
 finished = False
 while not finished:
     result = 0
@@ -31,6 +49,6 @@ while not finished:
     # Select the operation
     print('Result:', result)
     print('=================')
-    # Determine if the user has finished
+    finished = check_if_user_has_finished()
 
 print('Bye')

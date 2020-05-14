091ad30701dc23205b75c2911406d512ead6ac0c
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-05-14 19:47:21 +0200

    Add the modulus operation

--- calculator.py
+++ calculator.py
@@ -2,14 +2,22 @@ def add(x, y):
     """" Adds two numbers """
     return x + y
 
+
 def subtract(x, y):
     """ Subtracts two numbers """
     return x - y
+
 
 def multiply(x, y):
     """ Multiples two numbers """
     return x * y
 
+
 def divide(x, y):
     """Divides two numbers"""
     return x / y
+
+
+def modulus(x, y):
+    """Return the remainder of two numbers, after division"""
+    return x % y
--- test_calculator.py
+++ test_calculator.py
@@ -1,21 +1,30 @@
 import unittest
-from calculator import add, subtract, multiply, divide
+from calculator import (add, subtract, multiply, divide,
+                        modulus)
+
+
 class Calculator_Test(unittest.TestCase):
     """ """
+
     def test_add(self):
         """Test the add operation of the calculator."""
-        self.assertEqual(add(4,6), 10)
+        self.assertEqual(add(4, 6), 10)
 
     def test_subtract(self):
         """Test the subtract operation of the calculator."""
-        self.assertEqual(subtract(10,5), 5)
+        self.assertEqual(subtract(10, 5), 5)
 
     def test_multiply(self):
         """Test the multiply operation of the calculator."""
-        self.assertEqual(multiply(2,6), 12)
+        self.assertEqual(multiply(2, 6), 12)
 
-    def test_divid(self):
+    def test_divide(self):
         """Test the divide operation of the calculator."""
-        self.assertEqual(divide(9,3), 3)
+        self.assertEqual(divide(9, 3), 3)
+
+    def test_modulus(self):
+        """Test the divide operation of the calculator."""
+        self.assertEqual(modulus(12, 8), 4)
+
 
 unittest.main()

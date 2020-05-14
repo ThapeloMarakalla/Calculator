79b6206d895fede42f53ff56fd4e035f475921cc
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-05-14 19:26:03 +0200

    Add the subtract operation

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,13 @@
 import unittest
-from calculator import add
+from calculator import add, subtract
 class Calculator_Test(unittest.TestCase):
     """ """
     def test_add(self):
         """Test the add operation of the calculator."""
         self.assertEquals(add(4,6), 10)
+
+    def test_subtract(self):
+        """Test the subtract operation of the calculator."""
+        self.assertEquals(subtract(10,5), 5)
 
 unittest.main()
